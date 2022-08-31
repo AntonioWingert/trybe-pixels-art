504b63d1b9f2d618ca9e7a6a116495a7fae6e7d7
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-08-31 16:37:59 -0300

    continuando o projeto

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const buttonCreateRandomColor = document.querySelector('#button-random-color');
-buttonCreateRandomColor.addEventListener("click", function() {
-  const divsColor = document.querySelectorAll('.color');
-  for (let index = 0; index < divsColor.length; index += 1) {
-    if (divsColor[index] !== 'div.color.black') {
-      divsColor.classList.add();
-    };
-  }; 
-});
+const color2 = document.querySelector('#color2');
+const color3 = document.querySelector('#color3');
+const color4 = document.querySelector('#color4');
+const arrayColors = [color2, color3, color4];
+const localColours = JSON.parse(localStorage.getItem('colorPalette'));
+if (localColours != null) {
+  for (let index = 0; index < 3; index += 1) {
+    arrayColors[index].style.backgroundColor = localColours[arrayColors[index].id];
+  }
+}
+
+function random(min, max) {
+  const result = ((max - min) * Math.random()) + min;
+  return Math.floor(result);
+}
+
+function generateColors() {
+  const newColours = {};
+  for (let index = 0; index < 3; index += 1) {
+    const newColor = `rgb(${random(0, 255)}, ${random(0, 255)}, ${random(0, 255)})`;
+    arrayColors[index].style.backgroundColor = newColor;
+    newColours[arrayColors[index].id] = newColor;
+  }
+  localStorage.setItem('colorPalette', JSON.stringify(newColours));
+}
+
+buttonCreateRandomColor.addEventListener('click', generateColors);
+
